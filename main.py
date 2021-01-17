48331d251f3ca4a70014e7edd48e2e12f5b00687
Denniz Goren <dennizgoren@gmail.com>
2021-01-17 09:29:57 +0100

    Changes to the datastructure and algorithm

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from code.classes.protein import Protein
-from code.algorithms.random import random_directionlist
 from code.algorithms.random2 import random_algorithm
-import matplotlib.pyplot as plt
-import time, sys
-import numpy as np
-import random
-import csv
+
 
 
 if __name__ == "__main__":
 
-   
-     # Amino acid chain 
+    # Amino acid chain
     string = input("Provide amino acid chain:").upper()
 
     # Number of tries
@@ -21,14 +14,9 @@ if __name__ == "__main__":
     algo = random_algorithm(string, try_number)
     score_list = algo[1]
     highest_protein = algo[2]
-    
+
     # CSV output
     highest_protein.output()
-    
+
     # Visualisatie
     highest_protein.plot()
-    
-    
-
-  
-    
